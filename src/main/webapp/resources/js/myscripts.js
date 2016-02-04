e6033e30e22644dcb635a5db0bf189a0b4c6263b
Lev Buymistryuk <leoon5511@gmail.com>
2016-02-04 12:56:57 +0300

    view on all fields

--- src/main/webapp/resources/js/myscripts.js
+++ src/main/webapp/resources/js/myscripts.js
@@ -1,17 +1,32 @@
 function RandogramViewModel() {
      var self = this; 
+
     self.tags = ko.observable();
-    self.isSubscribed=ko.observable(true);
-    self.text = ko.observable();
+    self.isSubscribed=ko.observable(false);
+    self.isSearchByDate = ko.observable(true);
+    self.dateFrom = ko.observable();
+    self.dateTo = ko.observable();
+    self.resp = ko.observable();
+
     self.searchAction = function () {
-        //alert(""+self.isSubscribed());
+        alert("searchAction");
         $.ajax({
             url: "test",
             success: function (data) {
-                self.text(data);
+                self.resp(data);
             },
             dataType: "text"
         });
+    }
+    self.selectAction = function () {
+        alert("selectAction");
+        //$.ajax({
+        //    url: "test",
+        //    success: function (data) {
+        //        self.text(data);
+        //    },
+        //    dataType: "text"
+        //});
     }
 };
 var randogramViewModel = new RandogramViewModel();
